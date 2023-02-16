c5fc1240863017d09c3995f5dde77bee44eeb4a9
Mario Niko <marios-2222@hotmail.com>
2023-02-16 20:50:02 +0200

    desktop css

---
+++ app.js
@@ -0,0 +1,4 @@
+const cardNum = document.querySelector('.card-num')
+const cardNum = document.querySelector('.card-name')
+const cardNum = document.querySelector('.card-exp')
+const cardNum = document.querySelector('.card-cvc')
