091618cc2dc9e5963ae8004b76d4a6b5074ef61b
Mario Niko <marios-2222@hotmail.com>
2023-02-17 11:11:59 +0200

    final

--- app.js
+++ app.js
@@ -1,4 +1,30 @@
 const cardNum = document.querySelector('.card-num')
-const cardNum = document.querySelector('.card-name')
-const cardNum = document.querySelector('.card-exp')
-const cardNum = document.querySelector('.card-cvc')
+const cardName = document.querySelector('.card-name')
+const cardExp = document.querySelector('.card-exp')
+const cardCvc = document.querySelector('.card-cvc')
+const userName = document.querySelector('#name')
+const userNum = document.querySelector('#card-num')
+const userMonth = document.querySelector('#month')
+const userYear = document.querySelector('#year')
+const userCvc = document.querySelector('#cvc')
+
+
+userName.addEventListener("keyup",() => {
+cardName.innerText = userName.value
+})
+
+userNum.addEventListener("keyup",() => {
+cardNum.innerText = userNum.value
+})
+
+userMonth.addEventListener("keyup",() => {
+cardExp.innerText = `${userMonth.value}/${userYear.value}`
+})
+
+userYear.addEventListener("keyup",() => {
+cardExp.innerText = `${userMonth.value}/${userYear.value}`
+})
+
+userCvc.addEventListener("keyup",() => {
+cardCvc.innerText = userCvc.value
+})
